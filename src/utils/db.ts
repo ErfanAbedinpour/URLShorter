be2d41990008b0a8284fc1b70a9211a35b9f1814
erfan <erfan.wtf44@gmail.com>
2025-03-27 04:11:59 -0400

    chore: Adding Postgress Database Connection

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,12 @@
-import Redis from "ioredis";
+import { Client } from "pg";
 
-export function createRedisConnection(): Redis {
-	const redis = new Redis({ host: "localhost" });
-	return redis;
+async function createDatabaseConnection() {
+	const client = new Client({ host: 'localhost', port: 5432, password: "12341234", database: "url-shorter" });
+	try {
+		await client.connect();
+		return client;
+	} catch (err) {
+		console.error("error during connect redis ", err);
+		process.exit(1);
+	}
 }
