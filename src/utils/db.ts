f115e78fad76c25b00ad01b04ee8e88b0debbad3
erfan <erfan.wtf44@gmail.com>
2025-03-27 11:26:49 -0400

    fix: Fix top level await while creating Database

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,9 @@
 import { Client } from "pg";
 
-async function createDatabaseConnection() {
-	const client = new Client({ host: 'localhost', port: 5432, password: "12341234", database: "url-shorter" });
+export function createDatabaseConnection() {
+	const client = new Client({ host: 'localhost', port: 5432, password: "12341234", database: "url-shorter", user: 'admin' });
 	try {
-		await client.connect();
+		client.connect();
 		return client;
 	} catch (err) {
 		console.error("error during connect redis ", err);
