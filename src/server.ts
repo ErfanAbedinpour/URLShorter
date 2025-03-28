979ba727b80dbaa0ba9ea5eb8344511a6b22c5d2
erfan <erfan.wtf44@gmail.com>
2025-03-28 12:46:40 -0400

    chore: Adding CORS

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { convertToBase62 } from "./utils/convertToBase62";
 
 const fastify = Fastify({ logger: true });
 
+fastify.register(require("@fastify/cors"), { origin: "*" });
 
 fastify.post<{ Body: IBody }>("/", async (req, reply) => {
 	const { url } = req.body;
@@ -13,13 +14,12 @@ fastify.post<{ Body: IBody }>("/", async (req, reply) => {
 	if (!url)
 		return reply.send({ message: "url is not found in body" }).code(400);
 
-
 	try {
 		const isShortUrlExist = await urlService.isLongUrlExist(url);
 		if (isShortUrlExist)
 			return {
-				short_url: `${req.host}/${isShortUrlExist}`
-			}
+				short_url: `${req.host}/${isShortUrlExist}`,
+			};
 
 		const uniqueNumber = uniqueIdGenerator();
 		const uniqueBase62 = convertToBase62(uniqueNumber);
@@ -28,36 +28,39 @@ fastify.post<{ Body: IBody }>("/", async (req, reply) => {
 			id: uniqueNumber,
 			longUrl: url,
 			shortUrl: uniqueBase62,
-		})
-		return { shortUrl: `${req.host}/${uniqueBase62}` }
-
+		});
+		return { shortUrl: `${req.host}/${uniqueBase62}` };
 	} catch (err) {
-		console.error(err)
-		return reply.send({ message: "internal server error during create url" }).code(500)
+		console.error(err);
+		return reply
+			.send({ message: "internal server error during create url" })
+			.code(500);
 	}
 });
 
 fastify.get<{ Params: IParam }>("/:url", async (req, reply) => {
 	const { url } = req.params;
-	if (!url)
-		reply.send({ message: "enter url in param" }).code(404)
+	if (!url) reply.send({ message: "enter url in param" }).code(404);
 
 	try {
 		const long_url = await urlService.getOriginalUrlByShortUrl(url);
 
 		if (!long_url)
-			return reply.send({ message: "url does not found please first register it." }).code(404)
+			return reply
+				.send({ message: "url does not found please first register it." })
+				.code(404);
 
-		return reply.redirect(long_url, 301)
+		return reply.redirect(long_url, 301);
 	} catch (err) {
-		console.error(err)
-		return reply.send({ message: "url doest nof found please first register it." }).code(500)
+		console.error(err);
+		return reply
+			.send({ message: "url doest nof found please first register it." })
+			.code(500);
 	}
 });
 
-
 try {
-	fastify.listen({ port: +(process.env.PORT || 3000), host: '0.0.0.0' });
+	fastify.listen({ port: +(process.env.PORT || 3000), host: "0.0.0.0" });
 } catch (err) {
 	console.error(err);
 	process.exit(1);
